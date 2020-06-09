b5b66041ecdaebb40aebebc4470d44d18e8b02a8
ryotaro SUMI <ryo@suzusky.net>
2020-06-10 00:08:57 +0900

    [#10]create constructor

--- server/modules/ConstantSkill.php
+++ server/modules/ConstantSkill.php
@@ -6,9 +6,19 @@ class ConstantSkill implements Skill
     private $value;
     private $probability;
 
+    function __construct() {
+        $this->value = 0;
+        $this->probability = 1;
+    }
+
     public function set_value($_val)
     {
         $this->$value = $_val;
+    }
+
+    public function get_value()
+    {
+        $this->$value;
     }
 
     public function calc($param)

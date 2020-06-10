4eb2be4e46543a9fed75f49e4924dd9423500129
ss-koishi <yuuki100201@gmail.com>
2020-06-10 23:18:32 +0900

    base class Skill class

--- server/modules/Skill.php
+++ server/modules/Skill.php
@@ -1,12 +1,51 @@
 <?php
-abstract class Skill {
+abstract class Skill
+{
     /**
      * スキル発動確率
-     *
      * @var double
      */
     private $probability;
 
-    public abstract function calc($param);
+    /**
+     * 発動したかどうか
+     * @var bool
+     */
+    private $is_invoke;
+
+    /**
+     * スキルの影響対象
+     * @var array
+     */
+    private $targets;
+
+    abstract public function calc($param);
+    abstract public function invoke(); // 発動判定（確率とかメンバー限定とか）
+
+    /**
+     * 確率を設定 0 ~ 1
+     * @param double $prob 確率
+     */
+    public function set_probability(double $prob): void
+    {
+        $this->probability = $prob;
+    }
+
+    /**
+     * スキル対象の指定
+     * @param array $targets
+     */
+    public function set_targets(array $targets)
+    {
+        $this->targets = $targets;
+    }
+
+    /**
+     * スキルが発動したどうか
+     * @return bool スキルが発動したどうか
+     */
+    public function is_invoke(): bool
+    {
+        return $this->is_invoke;
+    }
 }
-?>

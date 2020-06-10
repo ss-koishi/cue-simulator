92430ab40d9201100da52afe0971ff5b845311b5
ss-koishi <yuuki100201@gmail.com>
2020-06-11 00:37:23 +0900

    imprive Skill.php

--- server/modules/Skill.php
+++ server/modules/Skill.php
@@ -14,13 +14,36 @@ abstract class Skill
     private $is_invoke;
 
     /**
+     * ３カット継続かどうか
+     * @var bool
+     */
+    private $is_keep;
+
+    /**
      * スキルの影響対象
      * @var array
      */
-    private $targets;
+    private $type;
+    private $team;
+    private $name;
+    private $target; // どの値があがるか ('all', 'voice', 'technique', 'mental', 'charisma')
 
     abstract public function calc($param);
-    abstract public function invoke(); // 発動判定（確率とかメンバー限定とか）
+
+    /**
+     * このスキルが発動するかどうかシミュレート
+     * @param  card_class メインキャスト全員
+     */
+    public function invoke($main_casts): void
+    {
+        $rand = $this->get_rand(0, 1);
+        if ($this->probability < $rand) { // 発動しない確率
+            $this->is_invoke = false;
+        }
+
+        // TODO: 対象声優の条件を満たすか
+        // タイプ、チーム etc...
+    }
 
     /**
      * 確率を設定 0 ~ 1
@@ -41,11 +64,31 @@ abstract class Skill
     }
 
     /**
+     * 3カット継続かどうか
+     * @param bool $is_keep
+     */
+    public function set_keep(bool $is_keep): void
+    {
+        $this->is_keep = $is_keep;
+    }
+
+    /**
      * スキルが発動したどうか
      * @return bool スキルが発動したどうか
      */
     public function is_invoke(): bool
     {
         return $this->is_invoke;
+    }
+
+    /**
+     * 乱数を取得
+     * @param  int    $min 最小値
+     * @param  int    $max 最大値
+     * @return double      [$min, $max]の乱数
+     */
+    protected function get_rand(int $min, int $max): double
+    {
+        return rand($min, $max);
     }
 }

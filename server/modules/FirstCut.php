7eb89f6a59161b6618f7a130480158df055ea454
ss-koishi <yuuki100201@gmail.com>
2020-06-20 21:45:35 +0900

    propotion skill valid

--- server/modules/FirstCut.php
+++ server/modules/FirstCut.php
@@ -1,6 +1,7 @@
 <?php
 require_once('Cut.php');
 require_once('ConstantSkill.php');
+require_once('PropotionSkill.php');
 
 class FirstCut extends Cut
 {
@@ -10,11 +11,14 @@ class FirstCut extends Cut
     public function start()
     {
         $skill = new ConstantSkill(156, 1.0, 'voice', ['all', 'all', 'all'], false);
+        $skill1 = new PropotionSkill(0.1, 1.0, 'voice', ['all', 'all', 'all'], false);
         // 発動するかどうか事前に判定
         $skill->invoke($this->main_casts);
+        $skill1->invoke($this->main_casts);
 
         // メインキャストがスキルの対象かどうか
         $skill->evaluate($this);
+        $skill1->evaluate($this);
 
         $this->actual_calc();
 
@@ -32,14 +36,25 @@ class FirstCut extends Cut
         var_dump($this->upward_sum);
         foreach($this->main_casts as $cast) {
             $index++;
+            $attr = $this->get_attr(); // カットの属性
+
+            // まずは定数アップから
             // add_allは無条件で呼び出す
             $cast->add_all($this->upward_sum[$index]['all'] + $this->keep_upward_sum[$index]['all']);
 
             // こっちは現カットの属性だけ足せば計算はあう
-            $action = 'add_' . $this->get_attr();
-            $cast->$action($this->upward_sum[$index][$this->get_attr()] + $this->keep_upward_sum[$index][$this->get_attr()]);
+            $action = 'add_' . $attr;
+            $cast->$action($this->upward_sum[$index][$attr] + $this->keep_upward_sum[$index][$attr]);
 
-            // TODO: 倍率バフ計算
+            // 倍率アップ
+            $getter = 'get_' . $attr;
+            $setter = 'set_' . $attr;
+
+            $propotion_sum_all = $this->propotion_sum[$index]['all'] + $this->keep_propotion_sum[$index]['all'];
+            $propotion_sum_attr = $this->propotion_sum[$index][$attr] + $this->keep_propotion_sum[$index][$attr];
+
+            // 必ず切り上げなので、1足してintにキャストすればok
+            $cast->$setter((int)(1 + $cast->$getter() * (1 + $propotion_sum_all + $propotion_sum_attr)));
         }
     }
 }
